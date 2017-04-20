c977ea272415c6265f3516912059173a7a43503a
mrossa93 <eossa93@hotmail.com>
2017-04-20 16:10:58 -0500

    Update Utilities
    - Method getIp
    - Method ping
    - Javadoc

--- src/app/Utilities.java
+++ src/app/Utilities.java
@@ -1,7 +1,53 @@
 package app;
 
+import java.net.InetAddress;
+
 /**
- * Created by eossa on 20/04/17.
+ * Class for utilities and useful methods.
+ *
+ * @author Elkin Fabian Ossa Zamudio
+ * @since 2017-04-20
  */
-public class Utilities {
+class Utilities {
+
+    /**
+     * Static method for get the host IP.
+     *
+     * @return The host ip.
+     */
+    static String getIp() {
+        String ip = "0.0.0.0";
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+//            System.out.println("Your Host addr: " + InetAddress.getLocalHost().getHostAddress());
+//            Enumeration<NetworkInterface> n = NetworkInterface.getNetworkInterfaces();
+//            while (n.hasMoreElements()) {
+//                NetworkInterface e = n.nextElement();
+//
+//                Enumeration<InetAddress> a = e.getInetAddresses();
+//                while (a.hasMoreElements()) {
+//                    InetAddress addr = a.nextElement();
+//                    System.out.println("  " + addr.getHostAddress());
+//                }
+//            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ip;
+    }
+
+    /**
+     * Method for verify if can ping to an specified IP address.
+     *
+     * @param destinationIp The destionation IP
+     * @return True if can ping, false if not.
+     */
+    static boolean ping(String destinationIp) {
+        try {
+            return InetAddress.getByName(destinationIp).isReachable(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
